07b11855c54582ba6c176de348be7053314bd612
smulyono <smulyono@me.com>
2013-06-15 20:21:14 -0700

    added cron and wakeup endpoint so the application will not need have long wakeup time when service user request (hack for spring mvc without reserved instance)

--- src/main/java/me/smulyono/gaeskeleton/controller/PageController.java
+++ src/main/java/me/smulyono/gaeskeleton/controller/PageController.java
@@ -3,15 +3,22 @@ package me.smulyono.gaeskeleton.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 public class PageController {
 	static Logger log = LoggerFactory.getLogger(PageController.class);
 	
+	@RequestMapping(value="/wakeup", method=RequestMethod.GET)
+	@ResponseBody
+	public String wakeup(Model model){
+		return "OK";
+	}
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public String home(ModelMap model){
